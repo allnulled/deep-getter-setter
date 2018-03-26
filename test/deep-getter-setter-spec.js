812ce66058f61da9d8d296b2cff2f54a46aac70f
allnulled <todosanulados@gmail.com>
2018-03-26 18:42:50 +0200

    Removed unnecessary logs.

--- test/deep-getter-setter-spec.js
+++ test/deep-getter-setter-spec.js
@@ -8,15 +8,13 @@ var {get,set,modify,exists,ensure} = DeepGetterSetter;
 var data = {a:{b:[0,5,10]}};
 
 // Check that that the getter works okay:
-console.log(get(data, ["a","b","1"]) === data["a"]["b"]["1"] ? "Passed -  getter 1!" : "Failed -  getter 1!");
+console.log(get(data, ["a","b","1"]) === data["a"]["b"]["1"] ? "Passed!" : "Failed -  getter 1!");
 
 // Use the setter:
 set(data, ["a","b","1"], 6);
 
-console.log(get(data, ["a","b","1"]));
-
 // Check that the setter worked good:
-console.log(get(data, ["a","b","1"]) === 6 ? "Passed -  setter 1!" : "Failed -  setter 1!");
+console.log(get(data, ["a","b","1"]) === 6 ? "Passed!" : "Failed -  setter 1!");
 
 // Use the modifier:
 modify(data, ["a","b","1"], function(parent, key) {
@@ -24,17 +22,17 @@ modify(data, ["a","b","1"], function(parent, key) {
 });
 
 // Check that the modifiers worked fine:
-console.log(get(data, ["a","b","1"]) === data["a"]["b"]["1"] ? "Passed -  modifier 1!" : "Failed -  modifier 1!");
+console.log(get(data, ["a","b","1"]) === data["a"]["b"]["1"] ? "Passed!" : "Failed -  modifier 1!");
 
 // Use the exists:
-console.log(exists(data, ["a", "b", "5"]) === false ? "Passed -  exists 1!":"Failed -  exists 1!");
+console.log(exists(data, ["a", "b", "5"]) === false ? "Passed!":"Failed -  exists 1!");
 
 // Check that the exists worked fine:
-console.log( (!(5 in data["a"]["b"])) ? "Passed -  exists 2!" : "Failed -  exists 2!");
+console.log( (!(5 in data["a"]["b"])) ? "Passed!" : "Failed -  exists 2!");
 
 // Use the ensurer:
 ensure(data, ["a", "b", "5"]);
 
 // Check that the ensurer worked fine:
-console.log(exists(data, ["a", "b", "5"]) === true ? "Passed -  ensurer 1!" : "Failed -  ensurer 1!");
-console.log(typeof (get(data, ["a", "b", "5"])) === "object" ? "Passed -  ensurer 2!" : "Failed -  ensurer 2!");
+console.log(exists(data, ["a", "b", "5"]) === true ? "Passed!" : "Failed -  ensurer 1!");
+console.log(typeof (get(data, ["a", "b", "5"])) === "object" ? "Passed!" : "Failed -  ensurer 2!");
